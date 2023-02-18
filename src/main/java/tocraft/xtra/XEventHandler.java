7ee9cc6275eaa73c16443ed19fb8c861fcfb8e15
Tobias Cuiper <tobias.cuiper@gmx.de>
2023-02-18 22:16:42 +0100

    updated potion of manified victory to show names

--- src/main/java/tocraft/xtra/XEventHandler.java
+++ src/main/java/tocraft/xtra/XEventHandler.java
@@ -1,19 +1,33 @@
 package tocraft.xtra;
 
+import java.util.List;
+
 import cpw.mods.fml.common.FMLLog;
 import cpw.mods.fml.common.eventhandler.EventPriority;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import net.minecraft.client.Minecraft;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.event.entity.living.LivingDeathEvent;
+import tocraft.xtra.items.PotionOfManifiedVictory;
 
 public class XEventHandler {
 	   
-	   @SubscribeEvent(priority = EventPriority.HIGHEST)
+	   @SubscribeEvent(priority = EventPriority.LOWEST)
 	   public void livingDeath(LivingDeathEvent event){
 		   if (event.entity instanceof EntityPlayer)
 	       {
-			   FMLLog.info("[To_Craft] Xtra: dropping playerFlesh");
-	           event.entity.dropItem(XRegistry.potionOfManifiedVictory, 1);
+			   PotionOfManifiedVictory victoryPotion = (PotionOfManifiedVictory)XRegistry.potionOfManifiedVictory;
+			   String playerName = ((EntityPlayer)event.entity).getDisplayName();		   
+			   ItemStack victoryPotionStack = new ItemStack(victoryPotion);
+			   
+	           List tooltip = victoryPotionStack.getTooltip(Minecraft.getMinecraft().thePlayer, false);
+	           victoryPotion.setPlayerName(victoryPotionStack, playerName);
+	           victoryPotion.addInformation(victoryPotionStack, Minecraft.getMinecraft().thePlayer, tooltip, false);
+	           
+			   FMLLog.warning("[To_Craft] Xtra: " + playerName + " died and dropped Potion of Manified Victory with " + victoryPotionStack.getTooltip(Minecraft.getMinecraft().thePlayer, false));
+	           
+	           event.entity.entityDropItem(victoryPotionStack, 1);
 	       }
 	   }
 }
